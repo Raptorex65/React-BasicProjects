79e976ddbf84e439abcca22fcd20460836d293db
Raptorex65 <71727239+Raptorex65@users.noreply.github.com>
2021-05-29 22:39:30 +0200

    "Commenting"

--- Rest-api-with-db_2nd_stemel/util/sequelize-db.js
+++ Rest-api-with-db_2nd_stemel/util/sequelize-db.js
@@ -27,17 +27,17 @@ db.students = Student(sequelize, Sequelize);
 db.enrolls = Enrollment(sequelize, DataTypes);
 
 // create relationship
-db.students.hasMany(db.lectures);
-db.teachers.hasMany(db.students);
+//db.students.hasMany(db.lectures); Bunlar extra tanimlamalar
+//db.teachers.hasMany(db.students); Bunlar extra tanimlamalar
 
 db.students.belongsToMany(db.lectures, { as: 'LecturesforStudent', through: db.enrolls, foreignKey: 'student_id'});
 db.lectures.belongsToMany(db.teachers, { as: 'TeachersinLectures', through: db.enrolls, foreignKey: 'lecture_id'});
 db.teachers.belongsToMany(db.students, { as: 'StudentsofTeachers', through: db.enrolls, foreignKey: 'teacher_id'});
 // The as key in the second argument's config object using belongsToMany is a way to specify the alias when we're doing Eager Loading / Include Queries
 
-  db.enrolls.belongsTo(db.students, { foreignKey: 'student_id', targetKey: 'student_id', as: 'Student' });
-  db.enrolls.belongsTo(db.teachers, { foreignKey: 'teacher_id', targetKey: 'teacher_id', as: 'Teacher' });
-  db.enrolls.belongsTo(db.lectures, { foreignKey: 'lecture_id', targetKey: 'lecture_id', as: 'Lecture' });
+db.enrolls.belongsTo(db.students, { foreignKey: 'student_id', targetKey: 'student_id', as: 'Student' });
+db.enrolls.belongsTo(db.teachers, { foreignKey: 'teacher_id', targetKey: 'teacher_id', as: 'Teacher' });
+db.enrolls.belongsTo(db.lectures, { foreignKey: 'lecture_id', targetKey: 'lecture_id', as: 'Lecture' });
 
 // use it to force to create the db from scratch 
 // .sync({ force: true })
